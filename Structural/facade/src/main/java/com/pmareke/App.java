29bf31f0e0beb2d38872cd3ac435ac21992ffc69
pmareke <pedro.lopez.mareque@gmail.com>
2019-08-11 17:20:42 +0200

    Add facade pattern

--- Structural/facade/src/main/java/com/pmareke/App.java
+++ Structural/facade/src/main/java/com/pmareke/App.java
@@ -1,7 +1,12 @@
 package com.pmareke;
 
+import com.pmareke.facades.ComputerFacade;
+import com.pmareke.models.Computer;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        ComputerFacade computer = new ComputerFacade(new Computer());
+        computer.turnOn(); // Ouch! Beep beep! Loading.. Ready to be used!
+        computer.turnOff(); // Bup bup buzzz! Haah! Zzzzz
     }
 }
--- Structural/facade/src/test/java/com/pmareke/AppTest.java
+++ Structural/facade/src/test/java/com/pmareke/AppTest.java
@@ -1,12 +1,44 @@
 package com.pmareke;
 
+import com.pmareke.facades.ComputerFacade;
+import com.pmareke.models.Computer;
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.*;
 
 public class AppTest {
+
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    public void shouldAnswerWithTurnOn() {
+        Computer computer = mock(Computer.class);
+
+        doNothing().when(computer).getElectricShock();
+        doNothing().when(computer).showLoadingScreen();
+        doNothing().when(computer).getElectricShock();
+        doNothing().when(computer).bam();
+
+        ComputerFacade computerFacade = new ComputerFacade(computer);
+        computerFacade.turnOn();
+
+        verify(computer, times(1)).getElectricShock();
+        verify(computer, times(1)).showLoadingScreen();
+        verify(computer, times(1)).getElectricShock();
+        verify(computer, times(1)).bam();
+    }
+
+    @Test
+    public void shouldAnswerWithTurnOff() {
+        Computer computer = mock(Computer.class);
+
+        doNothing().when(computer).closeEverything();
+        doNothing().when(computer).pullCurrent();
+        doNothing().when(computer).sooth();
+
+        ComputerFacade computerFacade = new ComputerFacade(computer);
+        computerFacade.turnOff();
+
+        verify(computer, times(1)).closeEverything();
+        verify(computer, times(1)).pullCurrent();
+        verify(computer, times(1)).sooth();
     }
 }
