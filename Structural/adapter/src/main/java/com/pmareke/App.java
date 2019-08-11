8712b1b9437b1eccc1c4a34d74ce3ce8ce3edb59
pmareke <pedro.lopez.mareque@gmail.com>
2019-08-11 08:35:45 +0200

    Fixed projects

---
+++ Structural/adapter/src/main/java/com/pmareke/App.java
@@ -0,0 +1,13 @@
+package com.pmareke;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Structural/facade/src/main/java/com/pmareke/App.java
@@ -0,0 +1,7 @@
+package com.pmareke;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
